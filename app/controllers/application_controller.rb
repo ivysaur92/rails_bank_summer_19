bc135faa340df00c44206ab1681aa4cb1e9ed081
Ivanna Brooks <ibrooks@its.nicholls.edu>
2019-06-03 20:28:45 -0600

    end of lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
+
+    # def user_params
+    #   params.require(:user).permit(:email, :encrypted_password, :first_name, :last_name)
+    # end
 end
